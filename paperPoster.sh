57c308e27de5a1cc7a2b11ec323b4f9f493e2c25
abigailbishop <ms.abigailbishop@gmail.com>
2020-01-06 12:50:13 -0600

    Removed paperPoster. Code movved to run_SlackPoster.py

--- paperPoster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-path="/Users/abigailbishop/Documents/GradSchool/Slack/slackPoster/lazy-astroph-"
-list='astroph hep wipac'
-
-for i in $list; do
-    rm /Users/abigailbishop/.lazy_astroph
-    $path$i/lazy_astroph.py -w $path$i/webhook $path$i/inputs
-done
