7cdc5b34c8657c9a189201aed8d2bae1462635ba
abigailbishop <ms.abigailbishop@gmail.com>
2020-01-08 11:00:59 -0600

    Added the new directories/channels to the script

--- run_slackPoster.py
+++ run_slackPoster.py
@@ -5,7 +5,11 @@ import os
 
 main_dict = {'astro': ['astro', 'physics'],
              'wipac': ['astro', 'physics', 'hep'],
-             'hep': ['hep', 'physics']}
+             'hep': ['hep', 'physics'],
+             'amo': ['physics'],
+             'biophysics' : ['physics'],
+             'condmat': ['cond', 'quant', 'physics'],
+             'plasma': ['physics']}
 
 
 for name, channels in main_dict.items():
