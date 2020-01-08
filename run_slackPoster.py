c77488dd0e669bfa5b2d7fe0a0dfa5ae7a63263d
Robert Morgan <robert.morgan@wisc.edu>
2020-01-08 07:52:43 -0600

    add line to remove the hidden file that was messing things up

--- run_slackPoster.py
+++ run_slackPoster.py
@@ -16,3 +16,5 @@ for name, channels in main_dict.items():
         
         os.system('./lazy_astroph.py -w {0}/webhook --channel {1} {0}/inputs'.format(name, channel))
         #os.system('./lazy_astroph.py --dry_run --channel {1} {0}/inputs'.format(name, channel))
+        
+        os.system('rm ../.lazy_astroph')
