8e1df2cb10889068b0d0abae39e9c3bf8652805e
Robert Morgan <robert.morgan@wisc.edu>
2020-04-01 17:08:56 -0500

    Includes Rob's PD events

--- PD_events/parsers.py
+++ PD_events/parsers.py
@@ -33,8 +33,6 @@ class Event():
 
         #use self.start_date and self.end_date when formatting the output
 
-        #BIG TODO
-
         month = self.start_date.month
         month_name = month_map(month, reverse=True).title()
 
@@ -72,32 +70,6 @@ class Event():
         output += str(end_hour) + ':'+ end_min_string + end_ampm
         
         return output
-
-    def parse_date(self, date):
-        current_date = datetime.datetime.now()
-        current_month = current_date.month
-        if date == "TBD":
-            return datetime.datetime(3000,3,3)
-        # For Rob's date format on the PGSC website
-        if len(date.split(';')) == 2 :
-            split_date = date.split(',')[0]
-            month = split_date.split(' ')[0]
-            day = split_date.split(' ')[1][:-2]
-            year = date.split(',')[-2]
-        # For the UWM Prof Dev events
-        else:
-            split_date = date.split(',')[0]
-            month = split_date.split(' ')[0]
-            day = split_date.split(' ')[1][:-2]
-        
-            if current_month == 12 and month_map[month.lower()] != 12:
-                year = current_date.year + 1
-            else:
-                year = current_date.year
-
-            event_date = datetime.datetime(year, 
-                                            month_map[month.lower()], int(day))
-            return event_date
 
 class UWMCareerDev():
     """Parses Events from UWM Career Development website """
@@ -148,8 +120,11 @@ class UWMCareerDev():
 
         events = []
         for title, subtitle, date, link in zip(titles, subtitles, dates, links):
-            start_date, end_date = self.parse_date(date)
-            events.append(Event(title.string, subtitle, start_date, end_date, link))
+            try:
+                start_date, end_date = self.parse_date(date)
+                events.append(Event(title.string, subtitle, start_date, end_date, link))
+            except:
+                continue
             
         return events
 
@@ -206,10 +181,16 @@ def filter_events(events):
     current_date = datetime.datetime.now()
     good_events = []
     allowed_range = datetime.timedelta(days=21)
+    #if event date < current date night night
     for event in events:
-        if event.end_date < current_date + allowed_range:
+        if (event.end_date < current_date + allowed_range and
+             event.end_date > current_date) :
             good_events.append(event)
-    return good_events
+    start_times = np.array([x.start_date for x in good_events])
+    return_events = []
+    for arg in np.argsort(start_times):
+        return_events.append(good_events[arg])
+    return return_events
 
 
 class PGSCProfDev():
@@ -235,20 +216,24 @@ class PGSCProfDev():
 
         soup = BeautifulSoup(response.text, "html.parser")
 
-        titles = [x for x in soup.findAll('h3') if str(x)[11:22] == 'event-title']
+        #titles = [x for x in soup.findAll('h3') if str(x)[11:22] == 'event-title']
+        titles = [x.string for x in soup.findAll('h3')]
         subtitles = [""]*len(titles)
-        python_dates = []
-        for p in [x.string for x in soup.findAll('p') if str(x)[11:25] == 'When and Where']:
-            python_dates.append(self.parse_date(p))
+        start_dates = []
+        end_dates = []
+        for p in [str(x) for x in soup.findAll('p') if str(x)[11:25] == 'When and Where']:
+            start_date, end_date = self.parse_date(p)
+            start_dates.append(start_date)
+            end_dates.append(end_date)
             
 
         links = self.urls*len(titles)
 
         if len(np.unique(
-                    [len(x) for x in [titles, subtitles, links, python_dates]])) != 1:
+                    [len(x) for x in [titles, subtitles, links, start_dates, end_dates]])) != 1:
 
             body = "{}\n".format(
-                           [len(x) for x in [titles, subtitles, links, python_dates]])
+                           [len(x) for x in [titles, subtitles, links, start_dates, end_dates]])
 
             with open('../emails.txt', 'r') as emails:
                 email_addresses = [x.strip() for x in emails.readlines()]
@@ -258,24 +243,43 @@ class PGSCProfDev():
                               "PD-poster@{}".format(platform.node()), mail)
 
         events = []
-        for title, subtitle, python_date, link in zip(titles, subtitles, python_dates, links):
-            events.append(Event(title.string, subtitle, python_date, link))
+        for title, subtitle, start_date, end_date, link in zip(
+                                     titles, subtitles, start_dates, end_dates, links):
+            events.append(Event(title.string, subtitle, 
+                                 start_date, end_date, link))
             
         return events
 
     def parse_date(self, date):
-        rawString = date.string
-        rawDate = rawString.split(";")
-        if rawDate[0][36:-4] == 'TBD':
-            return datetime.datetime(3000, 3, 3)
+        # Input Date: When and Where: April 11, 2020; 2:30-3:30; REMOTE
+        
+        if date.strip() == "<p><strong>When and Where:</strong> TBD</p>":
+            return datetime.datetime(3000, 3, 3), datetime.datetime(3000, 3, 3) 
 
-        rawDay = rawDate[0][36:-6]
-        rawYear = int(rawDate[0][-4:]) 
-        rawTime = rawDate[1] 
-        current_date = datetime.datetime.now()
-        current_year = current_date.year
-        if rawYear < current_year: rawYear = 3000
-        date.append(rawDay+', '+ ' ')        
+        rawDate = date[36:].strip().split(';')
+        # April 11, 2020;2:30-3:30;REMOTE
+
+        day = rawDate[0].split(',')[0].split(' ')[1]
+        month = rawDate[0].split(',')[0].split(' ')[0].lower()
+        month = month_map(month)
+        year = rawDate[0].split(',')[1].strip()
+        rawTime = rawDate[1].strip()
+        start_hour = int(rawTime.split('-')[0].split(':')[0])
+        start_min = rawTime.split('-')[0].split(':')[1][:2]
+        start_ampm = rawTime.split('-')[0].split(':')[1][2:]
+        end_hour = int(rawTime.split('-')[1].split(':')[0])
+        end_min = rawTime.split('-')[1].split(':')[1][:2]
+        end_ampm = rawTime.split('-')[1].split(':')[1][2:]
+
+        if start_ampm == 'pm' and start_hour != 12: start_hour += 12
+        if end_ampm == 'pm' and end_hour != 12: end_hour += 12
+
+        start = datetime.datetime(int(year), month, int(day), 
+                                     start_hour, int(start_min))
+        end = datetime.datetime(int(year), month, int(day), 
+                                     end_hour, int(end_min))
+ 
+        return start, end
 
 def report(body, subject, sender, receiver):
     """ send an email """
@@ -306,34 +310,11 @@ def run(string):
     return stdout0, stderr0, rc
 
 
-def slack_post(channel_body):
+def slack_post(channel_body, webhook):
     """ Styles a slack post and pushes it to slack """
     payload = {}
     payload["text"] = channel_body
     
     cmd = "curl -X POST --data-urlencode 'payload={}' {}".format(json.dumps(payload), webhook)
     run(cmd)
-
-parser = UWMCareerDev()
-
-with open('webhook', 'r') as webhook_file:
-    webhook = str(webhook_file.readline())
-
-startnum = 1
-for url in parser.urls:
-    events = parser.get_events(url)
-
-    events = filter_events(events)
-    
-    message_body = ''
-    for e in events:
-        event_info = str(startnum) + '. '
-        event_info += '*' + e.title + '*' + ': ' + e.subtitle
-        event_info += '\n\t\t' + e.display_date + ' - ' + e.link
-        message_body += event_info + '\n'
-        startnum += 1
-
-    slack_post(message_body)
-
-
 
