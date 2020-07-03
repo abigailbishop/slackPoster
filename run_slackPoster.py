c20b8087a751cf1f5e65c452976ee0018d5ca61c
Robert Morgan <robert.morgan@wisc.edu>
2020-07-03 10:57:20 -0500

    Moved 'break' up
    Co-authored-by: Abigail Bishop <ms.abigailbishop@gmail.com>

--- run_slackPoster.py
+++ run_slackPoster.py
@@ -29,9 +29,7 @@ for name, channels in main_dict.items():
      # For testing on personal slack channel
      #os.system('./lazy_astroph.py -w test_webhook --channel {1} {0}/inputs -m {2}'.format(name, channels, email_addresses))
      #os.system('./lazy_astroph.py -w test_webhook --channel {1} {0}/inputs'.format(name, channels))
+     #break
      
      # For running without updating param files or posting to Slack
      #os.system('./lazy_astroph.py --dry_run --channel {1} {0}/inputs'.format(name, channels))
-     
-     # Uncomment if testing
-     #break
