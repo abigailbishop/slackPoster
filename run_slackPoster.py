3815447ec3b8feb5b339e4482edc70ea46880042
Abigail Bishop <ms.abigailbishop@gmail.com>
2022-10-06 12:53:52 -0500

    Removing traces of PGSC Slack channels

--- run_slackPoster.py
+++ run_slackPoster.py
@@ -6,13 +6,7 @@ import os
 
 
 main_dict = {
-             'astro': 'astro,physics',
-             'wipac': 'astro,physics,hep',
-             'hep': 'hep,physics',
-             'amo': 'physics',
-             'biophysics' : 'physics',
-             'condmat': 'cond,quant,physics',
-             'plasma': 'physics,astro'
+             'wipac-papers': 'astro,physics,hep',
 }
 
 # read from gitignored-email file to get addresses
